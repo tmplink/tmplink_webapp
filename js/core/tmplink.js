6c912889ad12cface151ed1eca942589afa31656
TMPLINK STUDIO <45227137+tmplink@users.noreply.github.com>
2021-12-24 18:55:01 +0800

    Update tmplink.js

--- js/core/tmplink.js
+++ js/core/tmplink.js
@@ -45,7 +45,7 @@ class tmplink {
     upload_model_selected_val = 0
     download_retry = 0
     download_retry_max = 10
-    recaptcha_op = true
+    recaptcha_op = false
 
     constructor() {
         this.app_init();
