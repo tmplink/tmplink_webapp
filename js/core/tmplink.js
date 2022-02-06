960c12a46b7ddde3c3cd6b80ac3c5c3168b66c85
TMPLINK STUDIO <45227137+tmplink@users.noreply.github.com>
2022-02-07 02:16:09 +0800

    update

--- js/core/tmplink.js
+++ js/core/tmplink.js
@@ -1,14 +1,13 @@
 class tmplink {
 
     api_url = 'https://tun.tmp.link/api_v2'
-    api_backup = 'https://api.tmp.link/api_v2';
     api_url_upload = this.api_url + '/file'
     api_file = this.api_url + '/file'
     api_pay = this.api_url + '/pay'
     api_user = this.api_url + '/user'
     api_media = this.api_url + '/media'
     api_mr = this.api_url + '/meetingroom'
-    api_toks = this.api_backup + '/token'
+    api_toks = this.api_url + '/token'
     api_token = null
 
     pageReady = false
