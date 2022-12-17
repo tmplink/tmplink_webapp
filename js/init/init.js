b6c1d658ddc3768fab44962151695cc13f51b9bc
TMPLINK <45227137+tmplink@users.noreply.github.com>
2022-12-17 10:18:32 +0900

    update PWA

--- js/init/init.js
+++ js/init/init.js
@@ -2,13 +2,6 @@ app.ready(()=>{
     window.TL = new tmplink();
     TL.language(app.languageSetting);
     TL.languageData_init(app.languageData);
-
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker
-            .register("/pwa_sw.js")
-            .then(res => console.log("SW:ok"))
-            .catch(err => console.log("SW:error", err));
-    }
 });
 
 $('.tooltip').tooltipster({
