ac1922844cb73bf1e693ac35c25dfbde8c5fdf27
TMPLINK <45227137+tmplink@users.noreply.github.com>
2022-12-21 14:19:28 +0900

    コンソール出力のいくつかを削除しました。

--- js/init/init_index.js
+++ js/init/init_index.js
@@ -2,7 +2,6 @@ function INIT_index() {
     app.languageBuild();
     $('title').html(app.languageData.title_index);
     $('meta[name=description]').html(app.languageData.des_index);
-    console.log(app.languageData.des_index);
 
     if (TL.isLogin() === true) {
         dynamicView.workspace();
