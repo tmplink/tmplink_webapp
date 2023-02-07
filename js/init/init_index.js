5a48fe8924d26987b983ded1b4c9dc318e44a0c9
TMPLINK <45227137+tmplink@users.noreply.github.com>
2023-02-07 23:21:20 +0900

    version 1008

--- js/init/init_index.js
+++ js/init/init_index.js
@@ -1,9 +1,30 @@
-function INIT_index() {
-    app.languageBuild();
-    $('title').html(app.languageData.title_index);
-    $('meta[name=description]').html(app.languageData.des_index);
-
-    if (TL.isLogin() === true) {
-        dynamicView.workspace();
+app.ready(
+    () => {
+        let lang = app.languageSetting;
+        langset(lang);
+        app.languageBuild();
+        $('title').html(app.languageData.title_index);
+        $('meta[name=description]').html(app.languageData.des_index);
     }
+);
+
+function langset(lang) {
+    let span_lang = 'English';
+    if (lang === 'en') {
+        span_lang = 'English';
+    }
+
+    if (lang === 'cn') {
+        span_lang = '简体中文';
+    }
+
+    if (lang === 'hk') {
+        span_lang = '繁体中文';
+    }
+
+    if (lang === 'jp') {
+        span_lang = '日本語';
+    }
+    $('.selected_lang').html(span_lang);
+    app.languageSet(lang);
 }
