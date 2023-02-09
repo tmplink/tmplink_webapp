137fe85e68cb13b846ecb50f15680438af40de43
TMPLINK <45227137+tmplink@users.noreply.github.com>
2023-02-09 23:03:18 +0900

    fix an issue

--- pwa_sw.js
+++ pwa_sw.js
@@ -1,6 +1,6 @@
 const allowedTypes = ["js", "css", "jpg", "png", "webp", "woff", "svg", "gif", "ico", "ttf", "eot", "woff2", "html"];
 const allowedDomain = ["static.vx-cdn.com","tmp.link","ttttt.link","gstatic.com","www.recaptcha.net","127.0.0.1"];
-const resSet = "tmplink v15";
+const resSet = "tmplink v16";
 const assets = [
   '/',
 ];
