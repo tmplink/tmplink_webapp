f40bd944ae44f1d0620ead7b7b3a977784eed82a
TMPLINK <45227137+tmplink@users.noreply.github.com>
2024-07-18 11:22:07 +0900

    fix UI

--- pwa_sw.js
+++ pwa_sw.js
@@ -1,5 +1,5 @@
 const allowedDomain = ["cdn.code.so","www.tmp.link","www.ttttt.link","static-tmplink.vx-cdn.com"];
-const resSet = "tmplink-static-1280";
+const resSet = "tmplink-static-1281";
 const assets = [
   '/',
 ];
