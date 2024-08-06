db02b07f6e3147f68abd4292e574e571a0a33c81
TMPLINK <45227137+tmplink@users.noreply.github.com>
2024-08-06 10:35:30 +0900

    remove recaptcha

--- js/core/tmplink.js
+++ js/core/tmplink.js
@@ -51,7 +51,7 @@ class tmplink {
     upload_model_selected_val = 0
     download_retry = 0
     download_retry_max = 10
-    recaptcha_op = true
+    recaptcha_op = false
     recaptcha = '6LfqxcsUAAAAABAABxf4sIs8CnHLWZO4XDvRJyN5'
     recaptchaToken = '0'
 
--- pwa_sw.js
+++ pwa_sw.js
@@ -1,5 +1,5 @@
 const allowedDomain = ["cdn.code.so","www.tmp.link","www.ttttt.link","static-tmplink.vx-cdn.com"];
-const resSet = "tmplink-static-1292";
+const resSet = "tmplink-static-1293";
 const assets = [
   '/',
 ];
