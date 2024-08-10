d4fae5e462c28c217f2209368658b004395c3eb0
TMPLINK <45227137+tmplink@users.noreply.github.com>
2024-08-11 00:39:18 +0900

    remove google login for menubarX

--- js/init/init_login.js
+++ js/init/init_login.js
@@ -3,6 +3,13 @@ function INIT_login() {
         app.languageBuild();
         $('title').html(app.languageData.title_login);
         $('meta[name=description]').html(app.languageData.des_login);
+
+        //如果是从 menubarx 过来的应用，隐藏第三方登录
+        let mx = localStorage.getItem('from_menubarx');
+        if (mx) {
+            $('#google_login').hide();
+        }
+
         if (TL.isLogin()) {
             dynamicView.workspace();
         }
