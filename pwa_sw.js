7595ed277fe43396dd73ff284c4888a7ec12dcf1
TMPLINK <45227137+tmplink@users.noreply.github.com>
2024-09-29 09:56:00 +0900

    version updated

--- pwa_sw.js
+++ pwa_sw.js
@@ -1,5 +1,5 @@
 const allowedDomain = ["cdn.code.so","www.tmp.link","www.ttttt.link","static-tmplink.vx-cdn.com"];
-const resSet = "tmplink-static-1361";
+const resSet = "tmplink-static-1362";
 const assets = [
   '/',
 ];
