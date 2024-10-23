188eb69cba79be992084e024117792203172107c
TMPLINK <45227137+tmplink@users.noreply.github.com>
2024-10-23 16:26:48 +0900

    更新界面设定

--- pwa_sw.js
+++ pwa_sw.js
@@ -1,5 +1,5 @@
 const allowedDomain = ["cdn.code.so","www.tmp.link","www.ttttt.link","static-tmplink.vx-cdn.com"];
-const resSet = "tmplink-static-1366";
+const resSet = "tmplink-static-1367";
 const assets = [
   '/',
 ];
