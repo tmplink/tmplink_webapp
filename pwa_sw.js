8c5c4c4356a0b067fe4eaf29db1a22be6a53b603
TMPLINK <45227137+tmplink@users.noreply.github.com>
2024-11-08 11:32:45 +0900

    更新版本号至 1371

--- pwa_sw.js
+++ pwa_sw.js
@@ -1,5 +1,5 @@
 const allowedDomain = ["cdn.code.so","www.tmp.link","www.ttttt.link","static-tmplink.vx-cdn.com"];
-const resSet = "tmplink-static-1370";
+const resSet = "tmplink-static-1371";
 const assets = [
   '/',
 ];
