d6060a17422badbbd20cf0a0395ebb2b6772a419
TMPLINK <45227137+tmplink@users.noreply.github.com>
2024-12-10 12:53:09 +0900

    修复底部导航 UI 问题

--- pwa_sw.js
+++ pwa_sw.js
@@ -1,5 +1,5 @@
 const allowedDomain = ["cdn.code.so","www.tmp.link","www.ttttt.link","static-tmplink.vx-cdn.com"];
-const resSet = "tmplink-static-1500";
+const resSet = "tmplink-static-1501";
 const assets = [
   '/',
 ];
