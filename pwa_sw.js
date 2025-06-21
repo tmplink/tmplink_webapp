1fb16525a65b0aa2bdde98086e161dfab426f449
TMPLINK <45227137+tmplink@users.noreply.github.com>
2025-06-21 23:58:23 +0900

    fix: update resource set version to 1583 for improved caching

--- pwa_sw.js
+++ pwa_sw.js
@@ -1,5 +1,5 @@
 const allowedDomain = ["cdn.code.so","www.tmp.link","res.cntmp.link","www.ttttt.link","static-tmplink.vx-cdn.com"];
-const resSet = "tmplink-static-1582";
+const resSet = "tmplink-static-1583";
 const assets = [
   '/',
 ];
